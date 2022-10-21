8200b6722299d57b546df608826fdb51b0fda4a1
Trelel129 <ferrynur77@gmail.com>
2022-10-21 19:07:23 +0700

    init4

--- config.php
+++ config.php
@@ -1,10 +1,17 @@
 <?php
+// $mysqli = new mysqli("localhost","root","","etspweb");
+
+// // Check connection
+// if ($mysqli -> connect_errno) {
+//   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+//   exit();
+// }
 /* Database credentials. Assuming you are running MySQL server with default setting (user 'root' with no password) */
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'niagahoster');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'etspweb');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
