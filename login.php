ef4d8d6557e1187bf55e445c5081832bb6a3c35b
Trelel129 <ferrynur77@gmail.com>
2022-10-27 23:13:42 +0700

    small bugs

--- login.php
+++ login.php
@@ -122,7 +122,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-            <p> or </p>
             
         </form>
     </div>
